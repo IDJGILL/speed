3e7e38113bd57ee16d1743e835f3b22debfcfe47
IDJGILL <inderjeetgill.me@gmail.com>
2024-01-22 19:12:38 +0530

    sa

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -3,9 +3,6 @@ import {
   type DefaultSession,
   type NextAuthOptions,
 } from "next-auth";
-import DiscordProvider from "next-auth/providers/discord";
-
-import { env } from "~/env";
 
 /**
  * Module augmentation for `next-auth` types. Allows us to add custom properties to the `session`
@@ -44,10 +41,6 @@ export const authOptions: NextAuthOptions = {
     }),
   },
   providers: [
-    DiscordProvider({
-      clientId: env.DISCORD_CLIENT_ID,
-      clientSecret: env.DISCORD_CLIENT_SECRET,
-    }),
     /**
      * ...add more providers here.
      *
